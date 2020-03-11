385bfd9faddd4fdf4553ce161d48ef0dd90f4205
Kenny Parsons <kennyparsons93@gmail.com>
2020-03-10 21:22:08 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,12 +12,17 @@ function fuse_unmount {
   fusermount -u -z /mnt/$MOUNTPOINT
 }
 
+function makedir(){
+	mkdir -p /mnt/$MOUNTPOINT
+}
+
 if [ -z "${SOURCEDIRS}" ]; then
   echo "No filesystems specified!"
 fi
 
 trap term_handler SIGINT SIGTERM
 
+makdir
 while true
 do
   /usr/bin/mergerfs -f -o $OPTIONS "$SOURCEDIRS" /mnt/$MOUNTPOINT & wait ${!}
